769f27c8d7b96e9a01bef832106c9366cf97b96b
Mark Richman <mark@markrichman.com>
2019-01-18 07:44:30 -0500

    Fixed init script

--- Automating-AWS-Lambda-Python-Boto3/Scheduling-DynamoDB-Backups/custom_resource.py
+++ Automating-AWS-Lambda-Python-Boto3/Scheduling-DynamoDB-Backups/custom_resource.py
@@ -1,17 +1,20 @@
+import json
 import boto3
 import cfnresponse
 
 
 def lambda_handler(event, context):
-    try:
-        dynamodb = boto3.resource('dynamodb')
-        table = dynamodb.Table("Person")
-        table.put_item(
-            Item={'id': 1, 'last_name': 'Doe', 'first_name': 'John'})
-        table.put_item(
-            Item={'id': 2, 'last_name': 'Doe', 'first_name': 'Jane'})
-        table.put_item(
-            Item={'id': 3, 'last_name': 'Bloggs', 'first_name': 'Joe'})
-        cfnresponse.send(event, context, cfnresponse.SUCCESS, {})
-    except Exception:
-        cfnresponse.send(event, context, cfnresponse.FAILED, {})
+        print(json.dumps(event))
+        try:
+            dynamodb = boto3.resource('dynamodb')
+            table = dynamodb.Table("Person")
+            table.put_item(
+                Item={'id': 1, 'last_name': 'Doe', 'first_name': 'John'})
+            table.put_item(
+                Item={'id': 2, 'last_name': 'Doe', 'first_name': 'Jane'})
+            table.put_item(
+                Item={'id': 3, 'last_name': 'Bloggs', 'first_name': 'Joe'})
+            responseData = {}
+            cfnresponse.send(event, context, cfnresponse.SUCCESS, {})
+        except Exception:
+            cfnresponse.send(event, context, cfnresponse.FAILED, {})
